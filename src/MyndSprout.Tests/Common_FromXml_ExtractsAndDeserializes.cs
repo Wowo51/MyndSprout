0cb1094247adad0f8cdf5d75bebc9a014fa252ae
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:23 -0600

    Add src/MyndSprout.Tests/Common_FromXml_ExtractsAndDeserializes.cs

---
+++ src/MyndSprout.Tests/Common_FromXml_ExtractsAndDeserializes.cs
@@ -0,0 +1,15 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+[TestClass]
+public class Common_FromXml_ExtractsAndDeserializes
+{
+    [TestMethod]
+    public void FromXml_HandlesWrappedXml()
+    {
+        string xml = InputXmlSchemas.EmptyXsd();
+        Assert.IsFalse(string.IsNullOrEmpty(xml));
+    }
+}
+

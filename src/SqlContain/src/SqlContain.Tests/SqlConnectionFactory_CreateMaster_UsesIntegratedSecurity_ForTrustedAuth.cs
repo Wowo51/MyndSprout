5be3c525f844896773836d18c578f06785a8d459
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:45:12 -0600

    Add src/SqlContain/src/SqlContain.Tests/SqlConnectionFactory_CreateMaster_UsesIntegratedSecurity_ForTrustedAuth.cs

---
+++ src/SqlContain/src/SqlContain.Tests/SqlConnectionFactory_CreateMaster_UsesIntegratedSecurity_ForTrustedAuth.cs
@@ -0,0 +1,21 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Data.SqlClient;
+
+namespace SqlContain.Tests
+{
+    [TestClass]
+    public sealed class SqlConnectionFactory_CreateMaster_UsesIntegratedSecurity_ForTrustedAuth
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var o = new HardenerOptions { Server = "server", Auth = "Trusted" };
+            using var conn = SqlConnectionFactory.CreateMaster(o);
+            var csb = new SqlConnectionStringBuilder(conn.ConnectionString);
+            Assert.AreEqual("master", csb.InitialCatalog);
+            Assert.IsTrue(csb.IntegratedSecurity);
+            Assert.AreEqual("server", csb.DataSource);
+        }
+    }
+}

0daff9c3b38f0fdd7e198885ccbde1000c5113b7
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:52 -0600

    Add src/MyndSproutApp/RelayCommand.cs

---
+++ src/MyndSproutApp/RelayCommand.cs
@@ -0,0 +1,29 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+//Copyright Warren Harding 2025.
+using System;
+using System.Windows.Input;
+
+namespace MyndSproutApp
+{
+    public sealed class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _execute(parameter);
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}

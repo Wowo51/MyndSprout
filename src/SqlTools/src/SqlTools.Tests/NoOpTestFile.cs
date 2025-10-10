8c4b4ac4296aaa0de584f48ddac2b1f3d5c250c6
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:46:17 -0600

    Add src/SqlTools/src/SqlTools.Tests/NoOpTestFile.cs

---
+++ src/SqlTools/src/SqlTools.Tests/NoOpTestFile.cs
@@ -0,0 +1,2 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+// No-op test file.

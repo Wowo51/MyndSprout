2391ae5adb0555a094aea94f3597cad07a78d92a
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:15 -0600

    Add src/MyndSprout.Tests/Common_ExtractXml_Tests.cs

---
+++ src/MyndSprout.Tests/Common_ExtractXml_Tests.cs
@@ -0,0 +1,28 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class Common_ExtractXml_Tests
+    {
+        [TestMethod]
+        public void ExtractXml_ReturnsInnerXml_WhenWrapped()
+        {
+            var input = "prefix <?xml version=\"1.0\"?><root><a>1</a></root> suffix";
+            var xml = Common.ExtractXml(input);
+            Assert.IsNotNull(xml);
+            StringAssert.StartsWith(xml, "<root");
+            StringAssert.Contains(xml, "<a>1</a>");
+        }
+
+        [TestMethod]
+        public void ExtractXml_ReturnsNull_WhenNoXml()
+        {
+            var input = "no xml here";
+            var xml = Common.ExtractXml(input);
+            Assert.IsNull(xml);
+        }
+    }
+}

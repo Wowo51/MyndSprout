ba2be4d37a0a8d15aacad674fd4b025842da2ded
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:46:06 -0600

    Add src/SqlTools/src/SqlTools.Tests/FileRecordTests.cs

---
+++ src/SqlTools/src/SqlTools.Tests/FileRecordTests.cs
@@ -0,0 +1,18 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlTools;
+
+namespace SqlTools.Tests
+{
+    [TestClass]
+    public class FileRecordTests
+    {
+        [TestMethod]
+        public void Defaults_AreEmpty()
+        {
+            var r = new FileImporter.FileRecord();
+            Assert.AreEqual(string.Empty, r.Name);
+            Assert.AreEqual(string.Empty, r.Content);
+        }
+    }
+}

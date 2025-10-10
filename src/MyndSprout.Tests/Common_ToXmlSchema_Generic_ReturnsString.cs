96dd1ae5c5f38f95a6a2297ada04d96dff7a14b0
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:33 -0600

    Add src/MyndSprout.Tests/Common_ToXmlSchema_Generic_ReturnsString.cs

---
+++ src/MyndSprout.Tests/Common_ToXmlSchema_Generic_ReturnsString.cs
@@ -0,0 +1,15 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+[TestClass]
+public class Common_ToXmlSchema_Generic_ReturnsString
+{
+    [TestMethod]
+    public void ToXmlSchema_Generic_ReturnsNonEmpty()
+    {
+        string xml = InputXmlSchemas.EmptyXsd();
+        Assert.IsFalse(string.IsNullOrWhiteSpace(xml));
+    }
+}
+

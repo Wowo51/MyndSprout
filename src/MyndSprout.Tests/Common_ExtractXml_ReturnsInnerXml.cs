d3568b2c63eb7cb97d3fc1e87a840095c2b35cde
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:07 -0600

    Add src/MyndSprout.Tests/Common_ExtractXml_ReturnsInnerXml.cs

---
+++ src/MyndSprout.Tests/Common_ExtractXml_ReturnsInnerXml.cs
@@ -0,0 +1,28 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class Common_ExtractXml_ReturnsInnerXml
+    {
+        [TestMethod]
+        public void ExtractXml_HappyPath_ReturnsInnerXml()
+        {
+            string input = "prefix <?xml version=\"1.0\"?> <Root><Child>val</Child></Root> suffix";
+            var result = Common.ExtractXml(input);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains("<Root"));
+            Assert.IsTrue(result.Contains("<Child>val</Child>"));
+        }
+
+        [TestMethod]
+        public void ExtractXml_NoXml_ReturnsNull()
+        {
+            string input = "no xml here";
+            var result = Common.ExtractXml(input);
+            Assert.IsNull(result);
+        }
+    }
+}

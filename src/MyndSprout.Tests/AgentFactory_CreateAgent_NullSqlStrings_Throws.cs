df561eb2980235dcd1f514333264ac052b4484ab
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:39:53 -0600

    Add src/MyndSprout.Tests/AgentFactory_CreateAgent_NullSqlStrings_Throws.cs

---
+++ src/MyndSprout.Tests/AgentFactory_CreateAgent_NullSqlStrings_Throws.cs
@@ -0,0 +1,18 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class AgentFactory_CreateAgent_NullSqlStrings_Throws
+    {
+        [TestMethod]
+        public void CreateAgent_NullSqlStrings_Throws()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => AgentFactory.CreateAgent((SqlStrings)null!, 1));
+        }
+    }
+}
+

08c3e5db029ac549efba7c7717d2cb8f58875cc7
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:39:45 -0600

    Add src/MyndSprout.Tests/AgentFactory_CreateAgentFromConnectionString_Test.cs

---
+++ src/MyndSprout.Tests/AgentFactory_CreateAgentFromConnectionString_Test.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public sealed class AgentFactory_CreateAgentFromConnectionString_Test
+    {
+        [TestMethod]
+        public async Task CreateAgentFromConnectionString_localdb_master_returnsAgent()
+        {
+            string conn = "Server=(localdb)\\MSSQLLocalDB;Database=master;Integrated Security=True;TrustServerCertificate=True";
+            var agent = await MyndSprout.AgentFactory.CreateAgentFromConnectionStringAsync(conn, 1);
+            Assert.IsNotNull(agent);
+        }
+    }
+}

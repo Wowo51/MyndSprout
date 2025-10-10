90741ff53d0f218c9dd39f0c5bbedd9bca4325a2
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:16 -0600

    Add src/MyndSprout.Tests/SqlService_ExecuteQueryAsync_WithoutDatabase_ThrowsInvalidOperation.cs

---
+++ src/MyndSprout.Tests/SqlService_ExecuteQueryAsync_WithoutDatabase_ThrowsInvalidOperation.cs
@@ -0,0 +1,22 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class SqlService_ExecuteQueryAsync_WithoutDatabase_ThrowsInvalidOperation
+    {
+        [TestMethod]
+        public async Task ExecuteQueryAsync_WithoutDatabase_ThrowsInvalidOperation()
+        {
+            var s = new MyndSprout.SqlService();
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () =>
+            {
+                await s.ExecuteQueryAsync("select 1");
+            });
+        }
+    }
+}
+

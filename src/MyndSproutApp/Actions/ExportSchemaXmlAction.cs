396d40b770c94a58cdfeab47f0e045f230357d1b
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:43:27 -0600

    Add src/MyndSproutApp/Actions/ExportSchemaXmlAction.cs

---
+++ src/MyndSproutApp/Actions/ExportSchemaXmlAction.cs
@@ -0,0 +1,32 @@
+﻿//MIT license with supplement...
+using System;
+using System.Threading.Tasks;
+using MyndSprout;
+
+namespace MyndSproutApp.Actions
+{
+    public sealed class ExportSchemaXmlAction
+    {
+        private readonly Action<string> _log;
+        public ExportSchemaXmlAction(Action<string> log) => _log = log ?? (_ => { });
+
+        public async Task RunAsync(MainViewModel vm)
+        {
+            if (vm == null) throw new ArgumentNullException(nameof(vm));
+
+            string connStr = ConnectionStringHelper.BuildFromViewModel(vm);
+            var sql = new SqlStrings();
+
+            // Connect using the same string the app uses elsewhere
+            string connXml =
+                $"<ConnectInput><ConnectionString>{System.Security.SecurityElement.Escape(connStr)}</ConnectionString></ConnectInput>";
+            var connectResult = await sql.ConnectAsyncStr(connXml);
+
+            _log("ExportSchemaXml: connected. Fetching schema XML...");
+            string xml = await sql.GetSchemaAsyncStr("<Empty/>");
+
+            LogHelpers.LogBlock(_log, "ExportSchemaXml", xml);
+        }
+    }
+}
+

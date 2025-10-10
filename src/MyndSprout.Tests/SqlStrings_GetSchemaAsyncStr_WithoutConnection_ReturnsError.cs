98170bd10883770d4d1fa16e490d2f1851938eda
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:51 -0600

    Add src/MyndSprout.Tests/SqlStrings_GetSchemaAsyncStr_WithoutConnection_ReturnsError.cs

---
+++ src/MyndSprout.Tests/SqlStrings_GetSchemaAsyncStr_WithoutConnection_ReturnsError.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using MyndSprout;
+
+[TestClass]
+public class SqlStrings_GetSchemaAsyncStr_WithoutConnection_ReturnsError
+{
+    [TestMethod]
+    public async Task GetSchemaAsyncStr_WithoutConnection()
+    {
+        var sql = new SqlStrings();
+        var res = await sql.ExecuteToXmlAsyncStr("<sql/>");
+        Assert.IsTrue(res.Contains("success=\"false\"") || res.Contains("<Error") || !string.IsNullOrEmpty(res));
+    }
+}
+

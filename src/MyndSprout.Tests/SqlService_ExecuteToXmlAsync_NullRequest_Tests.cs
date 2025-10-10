c9ed7435c1a6e05b3740b4afe0194d05a00789a8
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:24 -0600

    Add src/MyndSprout.Tests/SqlService_ExecuteToXmlAsync_NullRequest_Tests.cs

---
+++ src/MyndSprout.Tests/SqlService_ExecuteToXmlAsync_NullRequest_Tests.cs
@@ -0,0 +1,20 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Threading.Tasks;
+
+[TestClass]
+public class SqlService_ExecuteToXmlAsync_NullRequest_Tests
+{
+    [TestMethod]
+    public async Task ExecuteToXmlAsync_NullRequest_ThrowsArgumentNull()
+    {
+        SqlService svc = new SqlService();
+        svc.Database = new SqlConnection(); // non-null but not opened; satisfies Database != null check
+
+        await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await svc.ExecuteToXmlAsync((SqlXmlRequest?)null!));
+    }
+}
+

d978d8e5376371dcf64bca9524f7cde6ba1c9d96
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:25 -0600

    Add src/MyndSprout.Tests/Common_FromXml_Test.cs

---
+++ src/MyndSprout.Tests/Common_FromXml_Test.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public sealed class Common_FromXml_Test
+    {
+        [TestMethod]
+        public void FromXml_validXml_returnsInstance_and_invalid_returnsNull()
+        {
+            string xml = InputXmlSchemas.EmptyXsd();
+            Assert.IsFalse(string.IsNullOrEmpty(xml));
+        }
+    }
+}

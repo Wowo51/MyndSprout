91aced8af6625ba5355359dac27281abd65c2197
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:52 -0600

    Add src/MyndSprout.Tests/InputXmlSchemas_EmptyXsd_ReturnsNotNull.cs

---
+++ src/MyndSprout.Tests/InputXmlSchemas_EmptyXsd_ReturnsNotNull.cs
@@ -0,0 +1,18 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public sealed class InputXmlSchemas_EmptyXsd_ReturnsNotNull
+    {
+        [TestMethod]
+        public void EmptyXsd_ReturnsNotNull()
+        {
+            var xsd = InputXmlSchemas.EmptyXsd();
+            Assert.IsNotNull(xsd);
+        }
+    }
+}
+

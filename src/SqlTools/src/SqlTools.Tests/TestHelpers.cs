fa6cf296fb9f6ca8944d6f41f2344154a2f87df1
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:46:23 -0600

    Add src/SqlTools/src/SqlTools.Tests/TestHelpers.cs

---
+++ src/SqlTools/src/SqlTools.Tests/TestHelpers.cs
@@ -0,0 +1,11 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+
+namespace SqlTools.Tests
+{
+    internal static class TestHelpers
+    {
+        // Minimal shared helpers/constants.
+        internal const string TempDbNamePrefix = "SqlToolsTests_";
+    }
+}

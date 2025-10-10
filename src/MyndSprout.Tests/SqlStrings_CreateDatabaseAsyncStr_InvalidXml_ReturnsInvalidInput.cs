541f36486537fece466c9d987bebafea9011fe44
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:37 -0600

    Add src/MyndSprout.Tests/SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput.cs

---
+++ src/MyndSprout.Tests/SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using MyndSprout;
+
+[TestClass]
+public class SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput
+{
+    [TestMethod]
+    public async Task CreateDatabaseAsyncStr_InvalidXml()
+    {
+        var sql = new SqlStrings();
+        var res = await sql.CreateDatabaseAsyncStr("not xml");
+        Assert.IsTrue(res.Contains("Invalid input XML"));
+    }
+}
+

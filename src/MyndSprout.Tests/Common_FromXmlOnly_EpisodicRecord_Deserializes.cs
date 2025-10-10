9adf47b4836b1432110ec9aa34a1d644859cb62e
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:20 -0600

    Add src/MyndSprout.Tests/Common_FromXmlOnly_EpisodicRecord_Deserializes.cs

---
+++ src/MyndSprout.Tests/Common_FromXmlOnly_EpisodicRecord_Deserializes.cs
@@ -0,0 +1,23 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class Common_FromXmlOnly_EpisodicRecord_Deserializes
+    {
+        [TestMethod]
+        public void FromXmlOnly_MinimalEpisodicRecord_Parses()
+        {
+            string guid = Guid.NewGuid().ToString();
+            string xml = $"<EpisodicRecord><EpisodeId>{guid}</EpisodeId><EpochIndex>2</EpochIndex></EpisodicRecord>";
+            var rec = Common.FromXmlOnly<EpisodicRecord>(xml);
+            Assert.IsNotNull(rec);
+            Assert.AreEqual(2, rec!.EpochIndex);
+            Assert.AreEqual(Guid.Parse(guid), rec.EpisodeId);
+        }
+    }
+}
+

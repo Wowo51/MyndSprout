bce066215efeeb62469d9ad18fced53f8b927190
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:44:48 -0600

    Add src/SqlContain/src/SqlContain.Tests/DenySkipped_AllowsWhenConfigured.cs

---
+++ src/SqlContain/src/SqlContain.Tests/DenySkipped_AllowsWhenConfigured.cs
@@ -0,0 +1,19 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlContain.Tests
+{
+    [TestClass]
+    public sealed class DenySkipped_AllowsWhenConfigured
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var opts = new SqlContain.HardenerOptions { AllowSkippedDeny = true };
+            string[] skipped = new[] { " DENY CREATE ASSEMBLY TO public; " };
+
+            // Should not throw when configured to allow skipped denies
+            SqlContain.DatabaseHardener.ThrowIfSkippedDenies(skipped, opts);
+        }
+    }
+}

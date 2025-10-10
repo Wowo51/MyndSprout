0dfc29efabed3d9418dd3ac29d9f6b9a44ab535d
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:45:10 -0600

    Add src/SqlContain/src/SqlContain.Tests/SqlConnectionFactory_CreateMaster_UsesCredentials_ForSqlAuth.cs

---
+++ src/SqlContain/src/SqlContain.Tests/SqlConnectionFactory_CreateMaster_UsesCredentials_ForSqlAuth.cs
@@ -0,0 +1,21 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Data.SqlClient;
+
+namespace SqlContain.Tests
+{
+    [TestClass]
+    public sealed class SqlConnectionFactory_CreateMaster_UsesCredentials_ForSqlAuth
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var o = new HardenerOptions { Server = "s", Auth = "Sql", User = "u", Password = "p" };
+            using var conn = SqlConnectionFactory.CreateMaster(o);
+            var csb = new SqlConnectionStringBuilder(conn.ConnectionString);
+            Assert.IsFalse(csb.IntegratedSecurity);
+            Assert.AreEqual("u", csb.UserID);
+            Assert.AreEqual("p", csb.Password);
+        }
+    }
+}

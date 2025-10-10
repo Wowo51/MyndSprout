9996ea68cf89a5e16a009693f229bf48fcb6defa
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:26 -0600

    Add src/MyndSproutApp/App.xaml.cs

---
+++ src/MyndSproutApp/App.xaml.cs
@@ -0,0 +1,15 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyndSproutApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

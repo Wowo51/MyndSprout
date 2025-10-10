5fefb41edd927a46a4415a7c0ac23560ba4402a6
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:55 -0600

    Add src/OpenAILLM/ChatReasoningEffortLevel.cs

---
+++ src/OpenAILLM/ChatReasoningEffortLevel.cs
@@ -0,0 +1,10 @@
+////MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+//namespace OpenAILLM
+//{
+//    public enum ChatReasoningEffortLevel
+//    {
+//        Low,
+//        Medium,
+//        High
+//    }
+//}

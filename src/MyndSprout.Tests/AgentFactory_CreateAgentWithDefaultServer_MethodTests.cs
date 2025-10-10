163508efe3d9cb128d46652e60b1dcff3e9375ac
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:39:48 -0600

    Add src/MyndSprout.Tests/AgentFactory_CreateAgentWithDefaultServer_MethodTests.cs

---
+++ src/MyndSprout.Tests/AgentFactory_CreateAgentWithDefaultServer_MethodTests.cs
@@ -0,0 +1,19 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class AgentFactory_CreateAgentWithDefaultServer_MethodTests
+    {
+        [TestMethod]
+        public async Task ValidDbNameWithOverride_ReturnsSqlAgent()
+        {
+            var agent = await AgentFactory.CreateAgentWithDefaultServer("TestDb", 1, @"Server=(localdb)\\MSSQLLocalDB;Database=master;");
+            Assert.IsNotNull(agent);
+            Assert.IsInstanceOfType(agent, typeof(SqlAgent));
+        }
+    }
+}

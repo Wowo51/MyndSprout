f939da6e92ab68184b5bbaff3327c044e36ecf28
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:18 -0600

    Add src/MyndSprout.Tests/Common_FromXmlOnly_DeserializesEmpty.cs

---
+++ src/MyndSprout.Tests/Common_FromXmlOnly_DeserializesEmpty.cs
@@ -0,0 +1,15 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+[TestClass]
+public class Common_FromXmlOnly_DeserializesEmpty
+{
+    [TestMethod]
+    public void FromXmlOnly_ParsesEmpty()
+    {
+        string xml = InputXmlSchemas.EmptyXsd();
+        Assert.IsFalse(string.IsNullOrEmpty(xml));
+    }
+}
+

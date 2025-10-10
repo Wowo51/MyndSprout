4400b7f04aafe30d2a0a265701217e32420f4830
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:44:56 -0600

    Add src/SqlContain/src/SqlContain.Tests/HardenerOptions_Validate_ThrowsWhenServerMissing.cs

---
+++ src/SqlContain/src/SqlContain.Tests/HardenerOptions_Validate_ThrowsWhenServerMissing.cs
@@ -0,0 +1,17 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlContain.Tests
+{
+    [TestClass]
+    public sealed class HardenerOptions_Validate_ThrowsWhenServerMissing
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var o = new HardenerOptions { Server = "", InternalFirewallOnly = false };
+            Assert.ThrowsException<ArgumentException>(() => o.Validate());
+        }
+    }
+}

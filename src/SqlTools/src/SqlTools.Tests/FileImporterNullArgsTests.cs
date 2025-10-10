05356bf2fa4ea4e6946f7d42e263accfbfc251bc
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:46:04 -0600

    Add src/SqlTools/src/SqlTools.Tests/FileImporterNullArgsTests.cs

---
+++ src/SqlTools/src/SqlTools.Tests/FileImporterNullArgsTests.cs
@@ -0,0 +1,32 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using SqlTools;
+
+namespace SqlTools.Tests
+{
+    [TestClass]
+    public class FileImporterNullArgsTests
+    {
+        [TestMethod]
+        public async Task EnsureTableAsync_NullConnection_Throws()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await FileImporter.EnsureTableAsync(null!));
+        }
+
+        [TestMethod]
+        public async Task ImportAsync_NullConnection_Throws()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await FileImporter.ImportAsync(null!, new[] { new FileImporter.FileRecord() }));
+        }
+
+        [TestMethod]
+        public async Task ImportAsync_NullFiles_Throws()
+        {
+            using var conn = new SqlConnection();
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await FileImporter.ImportAsync(conn, null!));
+        }
+    }
+}

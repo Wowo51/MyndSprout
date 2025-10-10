f82e584682476dbe6ff3fc4a812310e504a58206
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:32 -0600

    Add src/MyndSprout.Tests/SqlStrings_ConnectAsyncStr_BadConnection_Test.cs

---
+++ src/MyndSprout.Tests/SqlStrings_ConnectAsyncStr_BadConnection_Test.cs
@@ -0,0 +1,18 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public sealed class SqlStrings_ConnectAsyncStr_BadConnection_Test
+    {
+        [TestMethod]
+        public void ConnectAsyncStr_badConnection_returnsError()
+        {
+            var s = new MyndSprout.SqlStrings();
+            string input = "<ConnectInput><ConnectionString>Data Source=NOPE;Initial Catalog=none;Integrated Security=True</ConnectionString></ConnectInput>";
+            var xml = s.ConnectAsyncStr(input).GetAwaiter().GetResult();
+            Assert.IsTrue(xml.Contains("success=\"false\"") || xml.Contains("<Error>") || xml.Contains("Invalid"));
+        }
+    }
+}

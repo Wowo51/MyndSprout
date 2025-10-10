4ae892307294c83bd0d000dde6a3704dc2eadb55
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:09 -0600

    Add src/MyndSprout.Tests/SqlToXml_MakeSqlParameter_NullValue_ReturnsDBNull.cs

---
+++ src/MyndSprout.Tests/SqlToXml_MakeSqlParameter_NullValue_ReturnsDBNull.cs
@@ -0,0 +1,22 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Data.SqlClient;
+using MyndSprout;
+using System;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class SqlToXml_MakeSqlParameter_NullValue_ReturnsDBNull
+    {
+        [TestMethod]
+        public void MakeSqlParameter_NullValue_UsesDBNull()
+        {
+            var p = SqlToXml.MakeSqlParameter("@name", null);
+            Assert.IsNotNull(p);
+            Assert.AreEqual("@name", p.ParameterName);
+            Assert.AreEqual(DBNull.Value, p.Value);
+        }
+    }
+}
+

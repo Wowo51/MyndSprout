bbf4c00f97a9aff58a0f0602b5695799562e3af3
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:46:01 -0600

    Add src/SqlTools/src/SqlTools.Tests/ExportSchemaNullArgTests.cs

---
+++ src/SqlTools/src/SqlTools.Tests/ExportSchemaNullArgTests.cs
@@ -0,0 +1,18 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.Data.SqlClient;
+using SqlTools;
+
+namespace SqlTools.Tests
+{
+    [TestClass]
+    public class ExportSchemaNullArgTests
+    {
+        [TestMethod]
+        public void Export_NullConnection_Throws()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => ExportSchema.Export(null!));
+        }
+    }
+}

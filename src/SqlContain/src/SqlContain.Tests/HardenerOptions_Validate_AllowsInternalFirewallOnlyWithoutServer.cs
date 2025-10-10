61fa9b7db74ffb059ebededf35db4455c8acd496
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:44:53 -0600

    Add src/SqlContain/src/SqlContain.Tests/HardenerOptions_Validate_AllowsInternalFirewallOnlyWithoutServer.cs

---
+++ src/SqlContain/src/SqlContain.Tests/HardenerOptions_Validate_AllowsInternalFirewallOnlyWithoutServer.cs
@@ -0,0 +1,23 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlContain.Tests
+{
+    [TestClass]
+    public sealed class HardenerOptions_Validate_AllowsInternalFirewallOnlyWithoutServer
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var o = new SqlContain.HardenerOptions
+            {
+                Server = "",
+                InternalFirewallOnly = true,
+                Scope = SqlContain.Scope.Database,
+                Database = "master"
+            };
+            // Should not throw
+            o.Validate();
+        }
+    }
+}

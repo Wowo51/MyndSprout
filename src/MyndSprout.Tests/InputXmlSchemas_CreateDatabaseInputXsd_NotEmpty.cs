07788979db59a47fdc275fcff09f1210ada05e26
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:40:41 -0600

    Add src/MyndSprout.Tests/InputXmlSchemas_CreateDatabaseInputXsd_NotEmpty.cs

---
+++ src/MyndSprout.Tests/InputXmlSchemas_CreateDatabaseInputXsd_NotEmpty.cs
@@ -0,0 +1,15 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+[TestClass]
+public class InputXmlSchemas_CreateDatabaseInputXsd_NotEmpty
+{
+    [TestMethod]
+    public void CreateDatabaseInputXsd_ReturnsNonEmpty()
+    {
+        var s = InputXmlSchemas.CreateDatabaseInputXsd();
+        Assert.IsFalse(string.IsNullOrWhiteSpace(s));
+    }
+}
+

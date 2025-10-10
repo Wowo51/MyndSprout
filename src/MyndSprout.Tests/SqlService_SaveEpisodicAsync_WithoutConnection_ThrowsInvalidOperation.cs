8039610191cd21252e5516ad1d1f18364b95d53e
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:29 -0600

    Add src/MyndSprout.Tests/SqlService_SaveEpisodicAsync_WithoutConnection_ThrowsInvalidOperation.cs

---
+++ src/MyndSprout.Tests/SqlService_SaveEpisodicAsync_WithoutConnection_ThrowsInvalidOperation.cs
@@ -0,0 +1,19 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyndSprout;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public sealed class SqlService_SaveEpisodicAsync_WithoutConnection_ThrowsInvalidOperation
+    {
+        [TestMethod]
+        public void ExecuteQueryAsync_WithoutConnection_ThrowsInvalidOperation()
+        {
+            var svc = new SqlService();
+            Assert.ThrowsException<InvalidOperationException>(() =>
+                svc.ExecuteQueryAsync("SELECT 1").GetAwaiter().GetResult());
+        }
+    }
+}

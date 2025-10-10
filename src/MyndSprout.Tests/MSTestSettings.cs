b29b0f93697bb547ed176854f45fd54f27296fed
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:03 -0600

    Add src/MyndSprout.Tests/MSTestSettings.cs

---
+++ src/MyndSprout.Tests/MSTestSettings.cs
@@ -0,0 +1,4 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]

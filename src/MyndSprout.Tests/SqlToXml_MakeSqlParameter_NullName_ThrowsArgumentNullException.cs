a1b90a47caab0d4d3a8ed31a4f1981b865f88629
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:07 -0600

    Add src/MyndSprout.Tests/SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException.cs

---
+++ src/MyndSprout.Tests/SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyndSprout.Tests
+{
+    [TestClass]
+    public class SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException
+    {
+        [TestMethod]
+        public void MakeSqlParameter_NullName_ThrowsArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => SqlToXml.MakeSqlParameter(null!, 1));
+        }
+    }
+}
+

7b2eac1df7f17fa8b06b8f8e85df11b18a197b49
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:42:28 -0600

    Add src/MyndSproutApp/AssemblyInfo.cs

---
+++ src/MyndSproutApp/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
+                                                //(used if a resource is not found in the page,
+                                                // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
+                                                //(used if a resource is not found in the page,
+                                                // app, or any theme specific resource dictionaries)
+)]

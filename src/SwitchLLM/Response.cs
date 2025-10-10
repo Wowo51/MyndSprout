19a82ceaf375b00c6810c1f0e34d0915bcf6e519
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:43:16 -0600

    Add src/SwitchLLM/Response.cs

---
+++ src/SwitchLLM/Response.cs
@@ -0,0 +1,23 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+// Copyright Warren Harding 2025.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchLLM;
+
+public class Response
+{
+    public bool Succeeded = false;
+    public string Result = "";
+
+    public Response() { }
+
+    public Response(bool succeeded, string result)
+    {
+        Succeeded = succeeded;
+        Result = result;
+    }
+}

5f08b3b236c0c09a71030362dc800c7f0551fb1d
Warren Harding <warrencharding@yahoo.com>
2025-10-09 22:41:56 -0600

    Add src/MyndSprout.Tests/SqlToXml_ExecuteToXmlAsync_InvalidXml_ReturnsInvalidInput.cs

---
+++ src/MyndSprout.Tests/SqlToXml_ExecuteToXmlAsync_InvalidXml_ReturnsInvalidInput.cs
@@ -0,0 +1,17 @@
+﻿//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using MyndSprout;
+
+[TestClass]
+public class SqlToXml_ExecuteToXmlAsync_InvalidXml_ReturnsInvalidInput
+{
+    [TestMethod]
+    public async Task ExecuteToXmlAsync_InvalidXml()
+    {
+        var sql = new SqlStrings();
+        var res = await sql.ExecuteToXmlAsync("not xml");
+        Assert.IsTrue(res.Contains("Invalid input XML") || res.Contains("Invalid input XML"));
+    }
+}
+
